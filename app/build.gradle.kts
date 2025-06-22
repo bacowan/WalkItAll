ee3a96a51af882c72f316978e3471d22706f2a13
bacowan <cowan.brendan.a@gmail.com>
2025-06-22 20:43:10 +0900

    getting the initial position as the last known

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.maplibre.compose)
+    implementation(libs.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/walkitall/ui/mappage/MapPage.kt
+++ app/src/main/java/com/example/walkitall/ui/mappage/MapPage.kt
@@ -1,6 +1,9 @@
 package com.example.walkitall.ui.mappage
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -10,6 +13,7 @@ import androidx.compose.material.icons.filled.Stop
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -19,8 +23,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import com.example.walkitall.ui.theme.WalkItAllTheme
 import com.example.walkitall.utils.loadProperties
+import com.google.android.gms.location.LocationServices
 import com.maplibre.compose.MapView
 import com.maplibre.compose.camera.CameraState
 import com.maplibre.compose.camera.MapViewCamera
@@ -29,10 +35,13 @@ import com.maplibre.compose.rememberSaveableMapViewCamera
 
 @Composable
 fun MapPage(modifier: Modifier = Modifier) {
-    var isWalking by remember { mutableStateOf(false) }
-
     val context = LocalContext.current
-    val styleUrl = getStyleUrl(context)
+
+    var isWalking by remember { mutableStateOf(false) }
+    var location by remember { mutableStateOf<Location?>(null) }
+    val fusedLocationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
+
+    val styleUrl = remember { getStyleUrl(context) }
 
     var mapViewCamera = rememberSaveableMapViewCamera(
         initialCamera = MapViewCamera(
@@ -42,6 +51,20 @@ fun MapPage(modifier: Modifier = Modifier) {
             )
         )
     )
+
+    // Get the initial location based on the last known location
+    LaunchedEffect(Unit) {
+        val coarseLocationGranted = ContextCompat.checkSelfPermission(
+            context, Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        if (coarseLocationGranted) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { initialLocation: Location? ->
+                location = initialLocation
+            }
+        }
+    }
+
 
     Box(modifier = modifier.fillMaxSize()) {
         MapView(
@@ -76,20 +99,16 @@ fun MapPage(modifier: Modifier = Modifier) {
     }
 }
 
-@Composable
 private fun getStyleUrl(context: Context): String {
-    val styleUrl = remember {
-        val apiKey = try {
-            val properties = loadProperties(context)
-            properties.getProperty("MAPTILER_KEY")
-        } catch (_: Exception) {
-            // TODO: Exception handling
-            null
-        }
-        apiKey?.let { "https://api.maptiler.com/maps/streets/style.json?key=$it" }
-            ?: "https://demotiles.maplibre.org/style.json"
+    val apiKey = try {
+        val properties = loadProperties(context)
+        properties.getProperty("MAPTILER_KEY")
+    } catch (_: Exception) {
+        // TODO: Exception handling
+        null
     }
-    return styleUrl
+    return apiKey?.let { "https://api.maptiler.com/maps/streets/style.json?key=$it" }
+        ?: "https://demotiles.maplibre.org/style.json"
 }
 
 @Preview(showBackground = true)
